a0664e7bbe3013372b0e3898af9a8cf0edc185ac
@s.sirica2000 <s.sirica2000@gmail.com>
2025-05-30 12:57:49 +0200

    Secondo commit

--- src/oneccl/allreduce/allreduce.cpp
+++ src/oneccl/allreduce/allreduce.cpp
@@ -1,61 +1,119 @@
-/*
- Copyright 2016-2020 Intel Corporation
- 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- 
-     http://www.apache.org/licenses/LICENSE-2.0
- 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-#include "sycl_base.hpp"
+#include <iostream>
+#include <mpi.h>
+#include "oneapi/ccl.hpp"
+#include <chrono>                    // <<< aggiunto
+#include "common/include/arg_parser.hpp"
+#include <string>
 
-using namespace std;
-using namespace sycl;
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        cout << "usage ./sycl_allreduce_test [device]\n";
-        cout << "device could be 'cpu' or 'gpu'\n";
-        cout << "example: ./sycl_allreduce_test cpu\n";
-        exit(1);
+// Template wrapper for different data types
+template <typename T>
+void run_allreduce(size_t count, int size, int rank, ccl::communicator& comm, sycl::queue& q) {
+    // allocate device buffers
+    auto send_buf = sycl::malloc_device<T>(count, q);
+    auto recv_buf = sycl::malloc_device<T>(count, q);
+    // initialize buffers
+    auto e = q.submit([&](auto& h) {
+        h.parallel_for(count, [=](auto id) {
+            send_buf[id] = static_cast<T>(rank + id + 1);
+            recv_buf[id] = static_cast<T>(-1);
+        });
+    });
+    // compute expected sum
+    T check_sum = static_cast<T>(0);
+    for (int i = 1; i <= size; ++i) check_sum += static_cast<T>(i);
+    // perform allreduce
+    std::vector<ccl::event> deps{ccl::create_event(e)};
+    auto attr = ccl::create_operation_attr<ccl::allreduce_attr>();
+    auto t_start = std::chrono::high_resolution_clock::now();
+    ccl::allreduce(send_buf, recv_buf, count, ccl::reduction::sum, comm, q, attr, deps).wait();
+    auto t_end = std::chrono::high_resolution_clock::now();
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count();
+    std::cout << "Rank " << rank << " allreduce time: " << elapsed_ms << " ms\n";
+    // correctness check
+    sycl::buffer<T> check_buf(count);
+    q.submit([&](auto& h) {
+        sycl::accessor acc(check_buf, h, sycl::write_only);
+        h.parallel_for(count, [=](auto id) {
+            if (recv_buf[id] != static_cast<T>(check_sum + size * id)) acc[id] = static_cast<T>(-1);
+        });
+    });
+    q.wait_and_throw();
+    // print result
+    {
+        sycl::host_accessor acc(check_buf, sycl::read_only);
+        size_t i = 0;
+        for (; i < count; ++i) if (acc[i] == static_cast<T>(-1)) { std::cout << "FAILED\n"; break; }
+        if (i == count) std::cout << "PASSED\n";
     }
+    sycl::free(send_buf, q);
+    sycl::free(recv_buf, q);
+}
 
-    string device_type = argv[1];
+void mpi_finalize() {
+    int is_finalized = 0;
+    MPI_Finalized(&is_finalized);
 
-    if (device_type != "cpu" && device_type != "gpu") {
-        cout << "error: Invalid device '" << device_type << "'.\n";
-        cout << "device must be either 'cpu' or 'gpu'.\n";
-        exit(1);
+    if (!is_finalized) {
+        MPI_Finalize();
     }
+}
 
-    size_t count = 10 * 1024 * 1024;
+int main(int argc, char* argv[]) {
+    ArgParser parser(argc, argv);
+    parser.add<std::string>("--dtype").add<size_t>("--count");
+    parser.parse();
+    std::string dtype = parser.get<std::string>("--dtype");
+    size_t count = parser.get<size_t>("--count");
 
     int size = 0;
     int rank = 0;
 
     ccl::init();
 
-    MPI_Init(NULL, NULL);
+    MPI_Init(nullptr, nullptr);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     atexit(mpi_finalize);
 
-    test_args args(argc, argv, rank);
-
-    if (args.count != args.DEFAULT_COUNT) {
-        count = args.count;
+    /* find and initialize Level-Zero devices and queues */
+    std::vector<sycl::device> devices;
+    std::vector<sycl::queue> queues;
+    auto platform_list = sycl::platform::get_platforms();
+    for (const auto &platform : platform_list) {
+        auto platform_name = platform.get_info<sycl::info::platform::name>();
+        bool is_level_zero = platform_name.find("Level-Zero") != std::string::npos;
+        if (is_level_zero) {
+            std::cout << "Platform_name is:  " << platform_name << std::endl;
+            auto device_list = platform.get_devices();
+            for (const auto &device : device_list) {
+                if (device.is_gpu()) {
+                    devices.push_back(device);
+                }
+            }
+        }
     }
 
-    sycl::queue q;
-    if (!create_test_sycl_queue(device_type, rank, q, args))
-        return -1;
+    if (devices.size() < size) {
+        std::cerr << "Not enough devices for all ranks" << std::endl;
+        exit(-1);
+    }
+
+    sycl::context context(devices);
+    for (size_t i = 0; i < devices.size(); ++i) {
+        if (i == rank) { /* Only create a queue for the current rank's device */
+            queues.push_back(sycl::queue(context, devices[i], {sycl::property::queue::in_order()}));
+            break;
+        }
+    }
+
+    if (queues.empty()) {
+        std::cerr << "No queue created for rank " << rank << std::endl;
+        exit(-1);
+    }
+
+    /* Use the only queue in the queues vector for the current rank */
+    sycl::queue& q = queues[0];
 
     /* create kvs */
     ccl::shared_ptr_class<ccl::kvs> kvs;
@@ -63,10 +121,10 @@ int main(int argc, char *argv[]) {
     if (rank == 0) {
         kvs = ccl::create_main_kvs();
         main_addr = kvs->get_address();
-        MPI_Bcast((void *)main_addr.data(), main_addr.size(), MPI_BYTE, 0, MPI_COMM_WORLD);
+        MPI_Bcast((void*)main_addr.data(), main_addr.size(), MPI_BYTE, 0, MPI_COMM_WORLD);
     }
     else {
-        MPI_Bcast((void *)main_addr.data(), main_addr.size(), MPI_BYTE, 0, MPI_COMM_WORLD);
+        MPI_Bcast((void*)main_addr.data(), main_addr.size(), MPI_BYTE, 0, MPI_COMM_WORLD);
         kvs = ccl::create_kvs(main_addr);
     }
 
@@ -77,61 +135,16 @@ int main(int argc, char *argv[]) {
 
     /* create stream */
     auto stream = ccl::create_stream(q);
-
-    /* create buffers */
-    sycl::buffer<int> send_buf(count);
-    sycl::buffer<int> recv_buf(count);
-
-    {
-        /* open buffers and initialize them on the host side */
-        sycl::host_accessor send_buf_acc(send_buf, sycl::write_only);
-        sycl::host_accessor recv_buf_acc(recv_buf, sycl::write_only);
-        for (size_t i = 0; i < count; i++) {
-            send_buf_acc[i] = rank;
-            recv_buf_acc[i] = -1;
-        }
+    // dispatch based on dtype
+    if (dtype == "int") {
+        run_allreduce<int>(count, size, rank, comm, stream);
+    } else if (dtype == "float") {
+        run_allreduce<float>(count, size, rank, comm, stream);
+    } else if (dtype == "double") {
+        run_allreduce<double>(count, size, rank, comm, stream);
+    } else {
+        std::cerr << "Unsupported dtype: " << dtype << std::endl;
+        exit(-1);
     }
-
-    /* open send_buf and modify it on the device side */
-    q.submit([&](auto &h) {
-        sycl::accessor send_buf_acc(send_buf, h, sycl::write_only);
-        h.parallel_for(count, [=](auto id) {
-            send_buf_acc[id] += 1;
-        });
-    });
-
-    /* do not wait completion of kernel, dependency will be resolved by sycl::buffer */
-
-    /* invoke allreduce */
-    ccl::allreduce(send_buf, recv_buf, count, ccl::reduction::sum, comm, stream).wait();
-
-    /* open recv_buf and check its correctness on the device side */
-    q.submit([&](auto &h) {
-        sycl::accessor recv_buf_acc(recv_buf, h, sycl::write_only);
-        h.parallel_for(count, [=](auto id) {
-            if (recv_buf_acc[id] != size * (size + 1) / 2) {
-                recv_buf_acc[id] = -1;
-            }
-        });
-    });
-
-    if (!handle_exception(q))
-        return -1;
-
-    /* print out the result of the test on the host side */
-    {
-        sycl::host_accessor recv_buf_acc(recv_buf, sycl::read_only);
-        size_t i;
-        for (i = 0; i < count; i++) {
-            if (recv_buf_acc[i] == -1) {
-                std::cout << "FAILED\n";
-                break;
-            }
-        }
-        if (i == count) {
-            std::cout << "PASSED\n";
-        }
-    }
-
     return 0;
 }
